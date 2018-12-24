0904d5b45c7a300a877b9b4b64ead3080e82e289
calebvh <vanhaacf@mail.uc.edu>
2018-12-23 22:26:00 -0500

    Refactored the Organization of the Rolls and corrected the scoring calculation

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,16 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class Main {
 
-    public List Roll(){
+    public List Roll(int NumDice){
         Random rand = new Random();
         List<Integer> Outcome = new ArrayList<Integer>();
 
-        for(int i = 0; i<6; i++){
+        for(int i = 0; i<NumDice; i++){
             Outcome.add(i, rand.nextInt(6)+1);
         }
+
+        Collections.sort(Outcome);
 
         System.out.println("Your Roll: " +  Arrays.toString(Outcome.toArray()));
 
@@ -33,76 +32,100 @@ public class Main {
         return Straight;
     }
 
-    public Boolean IsThreeDoubles(List<Integer> Roll) {
-        List<Integer> DoubleChecker1 = new ArrayList<Integer>();
-        List<Integer> DoubleChecker2 = new ArrayList<Integer>();
-        List<Integer> AlreadyDouble = new ArrayList<Integer>();
+    public Boolean IsThreeDoubles(List<Integer> RollCount) {
+        int doubleCount = 0;
         boolean ret = false;
-        int DoubleCount = 0;
-        for (int j = 0; j < Roll.size(); j++) { //Iterate through rolled dice
-            if (DoubleChecker1.contains(Roll.get(j)) &&
-                    DoubleChecker2.contains(Roll.get(j)) &&
-                    !AlreadyDouble.contains(Roll.get(j))) {
-                DoubleCount++;
-                AlreadyDouble.add(Roll.get(j));
-            } else if (DoubleChecker1.contains(Roll.get(j)) &&
-                    !DoubleChecker2.contains(Roll.get(j))) {
-                DoubleChecker2.add(Roll.get(j));
-            } else {
-                DoubleChecker1.add(Roll.get(j));
+        for(int i = 1; i<RollCount.size(); i++){
+            if(RollCount.get(i) == 2){
+                doubleCount++;
             }
         }
 
-        if(DoubleCount == 3){
+        if(doubleCount == 3){
             ret = true;
         }
-
         return ret;
     }
 
-    public int CalculateScore(List<Integer> Roll){
+    public List CalculateScore(List<Integer> Roll, int CurrentRollScore){
         int PointsEarned = 0;
         ///////////////////////////////////////////////////////////////////
-        //Special Cases
-        //Straight
-        Boolean Straight = IsStraight(Roll);
-        Boolean Doubles = IsThreeDoubles(Roll);
+        //Number Arrays
+        List<Integer> MasterList =  new ArrayList<Integer>(Collections.nCopies(7,0));
+        MasterList.set(0,CurrentRollScore);
 
-        if(Straight){
-            PointsEarned = 5000;
+        for(int i = 0; i<Roll.size(); i++){
+           switch (Roll.get(i)){
+               case 1:
+                   MasterList.set(1,MasterList.get(1)+1);
+                   break;
+               case 2:
+                   MasterList.set(2,MasterList.get(2)+1);
+                   break;
+               case 3:
+                   MasterList.set(3,MasterList.get(3)+1);
+                   break;
+               case 4:
+                   MasterList.set(4,MasterList.get(4)+1);
+                   break;
+               case 5:
+                   MasterList.set(5,MasterList.get(5)+1);
+                   break;
+               case 6:
+                   MasterList.set(6,MasterList.get(6)+1);
+                   break;
+           }
         }
-        else if(Doubles){
-            PointsEarned = 500;
+        System.out.println(MasterList);
+        /////////////////////////////////////////////////////////////////////
+        // Calculate Score
+        if(IsStraight(Roll)){
+            MasterList.set(0, MasterList.get(0)+5000);
         }
-        else{
-            for(int i = 6; i>=1; i--){ //For Loop to focus on each number on the dice i.e 1-6
-                int numcount = 0;
-                for(int j = 0; j<6; j++){ //Iterate through rolled dice
-                    if(Roll.get(j) == i){
-                        numcount++;
-                    }
-                }
-                if(numcount>= 3){
-                    PointsEarned += (i*100) * numcount;
-                }
-                else if(i == 5 ){
-                    PointsEarned += numcount*50;
-                }
-                else if(i == 1){
-                    PointsEarned += numcount*100;
+        else if(IsThreeDoubles(MasterList))   {
+            MasterList.set(0, MasterList.get(0)+500);
+        }
+        else {
+            //Logic for One's Rolled
+            if (MasterList.get(1) < 3) {
+                PointsEarned += MasterList.get(1) * 100;
+            } else if (MasterList.get(1) >= 3) {
+                PointsEarned += MasterList.get(1) * 1000 * Math.pow(2, MasterList.get(1) - 3);
+            }
+
+            //Logic for Five's Rolled
+            if (MasterList.get(5) < 3) {
+                PointsEarned += MasterList.get(5) * 50;
+            }
+
+            //Logic for Every other Number
+            for (int i = 2; i < MasterList.size(); i++) {
+                if (MasterList.get(i) >= 3) {
+                    PointsEarned += i * 100 * Math.pow(2, MasterList.get(i) - 3);
                 }
             }
         }
 
-        System.out.println(PointsEarned);
-        return PointsEarned;
+         MasterList.set(0,MasterList.get(0)+PointsEarned);
+
+        System.out.println(MasterList.get(0));
+        return MasterList;
     }
 
     public static void main(String[] args){
         Main MainObj =  new Main();
-        List<Integer> PlayerRoll = MainObj.Roll();
-        //int[] a = new int[] {0, 0, 0, 0};
-        MainObj.CalculateScore(PlayerRoll);
+        List<Integer> PlayerRoll = MainObj.Roll(6);
+        /*
+        List<Integer> PlayerRoll =  new ArrayList<Integer>();
+        PlayerRoll.add(0,1);
+        PlayerRoll.add(1,2);
+        PlayerRoll.add(2,3);
+        PlayerRoll.add(3,4);
+        PlayerRoll.add(4,5);
+        PlayerRoll.add(5,6);
+        */
+
+        MainObj.CalculateScore(PlayerRoll, 0);
 
 
     }
