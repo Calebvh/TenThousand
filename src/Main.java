dbacaf8f770908c0830082c56c0768e24c5f8540
calebvh <vanhaacf@mail.uc.edu>
2018-12-24 00:24:46 -0500

    Player Scoring and Game Logic is Complete TODO: Win Conditions, and Dynamic Player Creation

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
+import java.awt.*;
 import java.util.*;
+import java.util.List;
+
 
 public class Main {
 
@@ -17,26 +20,22 @@ public class Main {
         return Outcome;
     }
 
-    public boolean IsStraight(List<Integer> Roll){
+    public boolean IsStraight(List<Integer> MasterRollList){
         Boolean Straight = true;
-        List<Integer> StraightChecker = new ArrayList<Integer>();
-        for(int j = 0; j<Roll.size(); j++){ //Iterate through rolled dice
-            if(StraightChecker.contains(Roll.get(j)) ){
-                Straight = false;
-            }
-            else{
-                StraightChecker.add(Roll.get(j));
+        for(int j = 1; j<MasterRollList.size()-1; j++){ //Iterate through rolled dice
+            if(MasterRollList.get(j) != 1){
+                Straight =false;
             }
 
         }
         return Straight;
     }
 
-    public Boolean IsThreeDoubles(List<Integer> RollCount) {
+    public Boolean IsThreeDoubles(List<Integer> MasterRollList) {
         int doubleCount = 0;
         boolean ret = false;
-        for(int i = 1; i<RollCount.size(); i++){
-            if(RollCount.get(i) == 2){
+        for(int i = 1; i<MasterRollList.size()-1; i++){
+            if(MasterRollList.get(i) == 2){
                 doubleCount++;
             }
         }
@@ -51,7 +50,7 @@ public class Main {
         int PointsEarned = 0;
         ///////////////////////////////////////////////////////////////////
         //Number Arrays
-        List<Integer> MasterList =  new ArrayList<Integer>(Collections.nCopies(7,0));
+        List<Integer> MasterList =  new ArrayList<Integer>(Collections.nCopies(8,0));
         MasterList.set(0,CurrentRollScore);
 
         for(int i = 0; i<Roll.size(); i++){
@@ -76,10 +75,9 @@ public class Main {
                    break;
            }
         }
-        System.out.println(MasterList);
         /////////////////////////////////////////////////////////////////////
         // Calculate Score
-        if(IsStraight(Roll)){
+        if(IsStraight(MasterList)){
             MasterList.set(0, MasterList.get(0)+5000);
         }
         else if(IsThreeDoubles(MasterList))   {
@@ -90,7 +88,7 @@ public class Main {
             if (MasterList.get(1) < 3) {
                 PointsEarned += MasterList.get(1) * 100;
             } else if (MasterList.get(1) >= 3) {
-                PointsEarned += MasterList.get(1) * 1000 * Math.pow(2, MasterList.get(1) - 3);
+                PointsEarned += 1000 * Math.pow(2, MasterList.get(1) - 3);
             }
 
             //Logic for Five's Rolled
@@ -99,34 +97,80 @@ public class Main {
             }
 
             //Logic for Every other Number
-            for (int i = 2; i < MasterList.size(); i++) {
+            for (int i = 2; i < MasterList.size()-1; i++) {
                 if (MasterList.get(i) >= 3) {
                     PointsEarned += i * 100 * Math.pow(2, MasterList.get(i) - 3);
+                }
+                else if(i != 5){
+                    MasterList.set(7, MasterList.get(7)+MasterList.get(i));
                 }
             }
         }
 
          MasterList.set(0,MasterList.get(0)+PointsEarned);
-
-        System.out.println(MasterList.get(0));
         return MasterList;
     }
 
+    public boolean RollAgain(List<Integer> CurrentRoll){
+        boolean retval = false;
+        System.out.println("Your total score for this turn is: "+ CurrentRoll.get(0));
+        System.out.println("You have "+ CurrentRoll.get(7)+" Dice to Roll");
+
+        System.out.println("Would you like to Roll Again?(y/n): ");
+        Scanner reader = new Scanner(System.in);  // Reading from System.in
+        String n = reader.next(); // Scans the next token of the input as an int.
+        //once finished
+        //reader.close();
+        System.out.println(n);
+        if(n.equals("y")){retval=true;}
+        return retval;
+    }
+
+    public void PlayerRoll(Player Player, int PrevRollScore, int DiceLeft){
+        System.out.println("#######################################");
+        System.out.println(Player.Name + "'s Roll" + "  " + Player.GotFirstThousand);
+        System.out.println("Current Score:" + Player.Score);
+        List<Integer> Roll = Roll(DiceLeft);
+        List<Integer> RollOutcome = CalculateScore(Roll, PrevRollScore);
+
+        if(PrevRollScore < RollOutcome.get(0)) {
+            boolean NewRoll = RollAgain(RollOutcome);
+            if (NewRoll) {
+                if(RollOutcome.get(7) == 0){
+                    PlayerRoll(Player, RollOutcome.get(0), 6);
+                }
+                else{
+                    PlayerRoll(Player, RollOutcome.get(0), RollOutcome.get(7));
+                }
+            } else {
+                if(!Player.GotFirstThousand && RollOutcome.get(0) >=1000){
+                    Player.GotFirstThousand = true;
+                    Player.Score = RollOutcome.get(0) - 1000;
+                }
+                else if(Player.GotFirstThousand){
+                    Player.Score += RollOutcome.get(0);
+                }
+            }
+        }
+        else{
+            System.out.println("Sorry you get no points this turn");
+        }
+    }
+
     public static void main(String[] args){
+
         Main MainObj =  new Main();
-        List<Integer> PlayerRoll = MainObj.Roll(6);
-        /*
-        List<Integer> PlayerRoll =  new ArrayList<Integer>();
-        PlayerRoll.add(0,1);
-        PlayerRoll.add(1,2);
-        PlayerRoll.add(2,3);
-        PlayerRoll.add(3,4);
-        PlayerRoll.add(4,5);
-        PlayerRoll.add(5,6);
-        */
-
-        MainObj.CalculateScore(PlayerRoll, 0);
-
-
+        Player Player1 = new Player("Caleb");
+        Player Player2 = new Player("Marieke");
+        int Turn = 0;
+        while(true) {
+            if(Turn%2 == 0){
+                MainObj.PlayerRoll(Player1, 0, 6);
+            }
+            else{
+                MainObj.PlayerRoll(Player2, 0, 6);
+            }
+            Turn++;
+        }
     }
 }
---
+++ src/Player.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Player {
+    String Name;
+    int Score;
+    boolean GotFirstThousand;
+    Player(String PlayerName){
+        Name = PlayerName;
+        Score = 0;
+        GotFirstThousand = false;
+    }
+
+}
